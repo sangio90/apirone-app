e7ddf9043a08606255bd584d11e0724e356f543a
Stan <stanisla.crivoseenco@studio.unibo.it>
2024-11-24 16:35:07 +0100

    ~ kendo draggable non funziona bene, useremo jQuery UI Draggable widget

--- apps/manager/assets/js/app-plate.js
+++ apps/manager/assets/js/app-plate.js
@@ -18,36 +18,11 @@ AP.plate.designer = (function () {
 	var pub = {};
 
 	pub.init = function () {
-		// $("#frutto-shuko").kendoDraggable({
-		// 	axis: "x",
-		// 	hint: function (element) {
-		// 		return element.clone();
-		// 	}
-		// });
-
-		// $("#draggable").data("kendoDraggable").bind("drag", function (e) {
-		// 	/* The result can be observed in the DevTools(F12) console of the browser. */
-		// 	console.log("x: ", e.screenX, "y: ", e.screenY);
-		// });
-
-		// Make the six divs inside the parent draggable
-		$(".grid-column").draggable({
-			containment: "#parent",
-			stop: function (event, ui) {
-				// Ensure only one section can be dragged at a time
-				$(".grid-column").draggable("option", "disabled", true);
-				$(this).draggable("option", "disabled", false);
-			}
-		});
-
-		// Make the horizontal draggable div
 		$("#draggable-plate-item").draggable({
 			axis: "x", // Only allow horizontal dragging
 			containment: "#plate-grid",
 			grid: [50]
 		});
-
-
 	};
 
 	return pub;
