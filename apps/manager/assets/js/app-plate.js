0cb21c816adde9c4cfd5a5c9b5340bcfd630fd81
Stan <stanisla.crivoseenco@studio.unibo.it>
2024-11-30 17:15:27 +0100

    + avanzamento designer delle placche

--- apps/manager/assets/js/app-plate.js
+++ apps/manager/assets/js/app-plate.js
@@ -17,43 +17,119 @@ $(document).ready(function () {
 AP.plate.designer = (function () {
 	var pub = {};
 
-	const PLATE_ITEM_WIDTH_IN_PX = 100;
-	const ONE_STEP_WIDTH = PLATE_ITEM_WIDTH_IN_PX / 2;
-	const MIN_DISTANCE_BEFORE_DRAGGING = 0;
-
-	// TODO: la funzione deve restituire le coordinate per un array del tipo { start: 0, end: 0 }, oppure { start: 0, end: 1 } per i frutti che occupano piu' spazio
-	function parseCurrentPosition(uiElement) {
-		return {
-			start: uiElement.position.offsetInPx / ONE_STEP_WIDTH,
-			end: uiElement.data("element-dimension"),
-		};
-	}
-
 	pub.init = function () {
-		$("#draggable-plate-item").draggable({
-			axis: "x",
+		const CELL_TYPES = {
+			EMPTY: "_",
+			PROHIBITED: "0",
+		};
+		const CELL_SIZE_IN_X = pageData.GRID_CELL_DIMENSIONS.WIDTH;
+		const CELL_SIZE_IN_Y = pageData.GRID_CELL_DIMENSIONS.HEIGHT;
+
+		const plateGrid = pageData.PLATE_GRID;
+
+		const MIN_DISTANCE_BEFORE_DRAGGING = 0;
+
+		function parseBoundingBox(uiElement) {
+			const elementBounds = uiElement.helper[0].getBoundingClientRect();
+
+			const result = {
+				id: uiElement.helper.attr("id"),
+				x1: uiElement.position.left / CELL_SIZE_IN_X,
+				x2: null,
+				y1: uiElement.position.top / CELL_SIZE_IN_Y,
+				y2: null,
+				width: elementBounds.width / CELL_SIZE_IN_X,
+				height: elementBounds.height / CELL_SIZE_IN_Y,
+			};
+
+			result.x2 = result.x1 + result.width;
+			result.y2 = result.y1 + result.height;
+
+			return result;
+		}
+
+		function isColliding(box, grid) {
+			let result = false;
+
+			// Broad phase
+			const xAxis = { start: box.x1, end: box.x2 };
+			const yAxis = { start: box.y1, end: box.y2 };
+			const gridSection = plateGrid.slice(yAxis.start, yAxis.end).map(inner => inner.slice(xAxis.start, xAxis.end));
+			const possibleBoxes = {};
+			for (const row of gridSection) {
+				for (const cell of row) {
+					if (cell != box.id && cell != CELL_TYPES.EMPTY) {
+						if (!possibleBoxes.hasOwnProperty(cell)) {
+							possibleBoxes[cell] = pageData.PLATE_ELEMENTS[cell];
+						}
+					}
+				}
+			}
+
+			// Narrow phase
+			for (const [key, candidateBox] of Object.entries(possibleBoxes)) {
+				if (
+					candidateBox.x1 <= box.x2 && candidateBox.x2 >= box.x1
+					&&
+					candidateBox.y1 <= box.y2 && candidateBox.y2 >= box.y1
+				) {
+					result = true;
+
+					break;
+				}
+			}
+
+			return result;
+		}
+
+		function canSwap(box, grid) {
+			let result = false;
+
+			return result;
+		}
+
+		$(".draggable-plate-item").draggable({
+			// axis: "x",
 			containment: "#plate-grid",
 			distance: MIN_DISTANCE_BEFORE_DRAGGING,
-			grid: [ONE_STEP_WIDTH],
+			grid: [CELL_SIZE_IN_X, CELL_SIZE_IN_Y],
 			// revert: true,
 			revertDuration: 250,
 			start: function(event, ui) {
-				console.log("start", event, ui);
+				// console.log("start", event, ui);
+
+				ui.helper.addClass("is-dragging");
 			},
 			drag: function (event, ui) {
 				// TODO: qui bisogna inserire la logica di controllo durante lo spostamento, ovvero, se ci sono degli altri frutti
 				// devo capire se posso switch'are le loro posizioni oppure devo mettere indietro il draggable perche' manca la distanza minima
-				console.log("drag", ui.position);
 				// Solo in questo metodo posso modificare la posizione del draggable con il metodo:
 				// ui.position.left = [NUMERO_PIXEL];
+
+				const newBoundingBox = parseBoundingBox(ui);
+				// console.log(newBoundingBox.x1, newBoundingBox.x2, newBoundingBox.y1, newBoundingBox.y2);
+
+				if (isColliding(newBoundingBox, plateGrid)) {
+					ui.helper.addClass("is-colliding");
+
+					if (canSwap(newBoundingBox, plateGrid)) {
+						console.log("can swap");
+
+						ui.helper.removeClass("is-not-swappable");
+						// swapWithCollidingBox(newBoundingBox);
+					} else {
+						ui.helper.addClass("is-not-swappable");
+					}
+				} else {
+					ui.helper.removeClass("is-colliding");
+				}
 			},
 			stop: function (event, ui) {
-				console.log("stop", event, ui);
-				// TODO: la prossima volta
-				// const newPositionIndex = parseCurrentPosition(ui);
-				// console.log(newPositionIndex);
-				// ui.helper.attr("data-start-position-index", newPositionIndex.start);
-				// ui.helper.attr("data-end-position-index", newPositionIndex.end);
+				// console.log("stop", event, ui);
+
+				// TODO: devo aggiornare la plateGrid, se le coordinate finali sono diverse da quelle di partenza
+				ui.helper.removeClass("is-dragging");
+				ui.helper.removeClass("is-not-swappable");
 			},
 		});
 	};
--- apps/manager/views/plate/designer.cfm
+++ apps/manager/views/plate/designer.cfm
@@ -1,3 +1,38 @@
+<cfscript>
+	GRID_CELL_DIMENSIONS = {
+		"WIDTH" = 25, // in px
+		"HEIGHT" = 45, // in px
+	};
+	// Data from backend
+	// LEGEND:
+	// "A - Z" - Module Id (PK in DataBase)
+	// "_" - empty free space
+	// "0" - prohibited space
+	PLATE_GRID = [
+		["A", "A", "A", "A", "B", "B"],
+		["A", "A", "A", "A", "B", "B"],
+		["_", "_", "_", "_", "_", "_"],
+		["_", "_", "_", "_", "_", "_"],
+	];
+
+	PLATE_ELEMENTS = {
+		A = {
+			"imgSrc" = "/assets/main/img/foto_frutto_schuko.png",
+			"x1" = 0,
+			"y1" = 0,
+			"x2" = 3,
+			"y2" = 1,
+		},
+		B = {
+			"imgSrc" = "/assets/main/img/foto_frutto_bipasso.png",
+			"x1" = 4,
+			"y1" = 0,
+			"x2" = 5,
+			"y2" = 1,
+		},
+	};
+</cfscript>
+
 <cfoutput>
 	<div id="plate-designer-root">
 		<div>
@@ -20,36 +55,48 @@
 
 		<div class="plate-designer">
 			<div class="plate-background" style="background-image: url('/assets/main/img/foto_placca.png');">
-				<div id="plate-grid">
-					<div class="grid-column p1">
-						<span class="position-label">Pos 1</span>
-					</div>
-					<div class="grid-column p2">
-						<span class="position-label">Pos 2</span>
-					</div>
-					<div class="grid-column p3">
-						<span class="position-label">Pos 3</span>
-					</div>
-					<div class="grid-column p4">
-						<span class="position-label">Pos 4</span>
-					</div>
-					<div class="grid-column p5">
-						<span class="position-label">Pos 5</span>
-					</div>
-					<div class="grid-column p6">
-						<span class="position-label">Pos 6</span>
-					</div>
+				<div
+					id="plate-grid"
+					style="grid-auto-rows: #GRID_CELL_DIMENSIONS.HEIGHT#px; grid-auto-columns: #GRID_CELL_DIMENSIONS.WIDTH#px;"
+					>
+					<cfloop from="1" to="#arrayLen(PLATE_GRID)#" index="y">
+						<cfloop from="1" to="#arrayLen(PLATE_GRID[y])#" index="x">
+							<div
+								class="grid-column p#x#"
+								style="grid-row: #y# / #y + 1#; grid-column: #x# / #x + 1#;"
+								>
+								<span class="position-label" style="font-size: 8px;">
+									(#x#, #y#)
+								</span>
+							</div>
+						</cfloop>
+					</cfloop>
 
-					<div
-						id="draggable-plate-item"
-						data-element-dimension="2"
-						data-start-position-index="0"
-						data-end-position-index="1"
-						>
-						<img style="width: 100%; height: 100%; object-fit: fill;" id="foto_frutto_shuko" src="/assets/main/img/foto_frutto.png">
-					</div>
+					<cfloop collection="#PLATE_ELEMENTS#" item="elementKey">
+						<cfscript>
+							element = PLATE_ELEMENTS[elementKey];
+						</cfscript>
+
+						<div
+							id="#elementKey#"
+							class="draggable-plate-item"
+							style="left: #GRID_CELL_DIMENSIONS.WIDTH * element.x1#px; top: #GRID_CELL_DIMENSIONS.HEIGHT * element.y1#px; width: #GRID_CELL_DIMENSIONS.WIDTH * (element.x2 - element.x1 + 1)#px; height: #GRID_CELL_DIMENSIONS.HEIGHT * (element.y2 - element.y1 + 1)#px;"
+							>
+							<img
+								src="#element.imgSrc#"
+								style="width: 100%; height: 100%; object-fit: fill;"
+								>
+						</div>
+					</cfloop>
 				</div>
 			</div>
 		</div>
 	</div>
+
+	<script>
+		pageData = {};
+		pageData.GRID_CELL_DIMENSIONS = #serializeJSON(GRID_CELL_DIMENSIONS)#;
+		pageData.PLATE_GRID = #serializeJSON(PLATE_GRID)#;
+		pageData.PLATE_ELEMENTS = #serializeJSON(PLATE_ELEMENTS)#;
+	</script>
 </cfoutput>
