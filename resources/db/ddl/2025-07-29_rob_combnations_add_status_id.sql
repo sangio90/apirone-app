f66db7363357af79794343ee141deabc2525f43d
Roberto Marzialetti <roberto@marzialetti.com>
2025-07-29 12:52:24 +0200

    set default and not null to combinations.status_id

--- resources/db/ddl/2025-07-29_rob_combnations_add_status_id.sql
+++ resources/db/ddl/2025-07-29_rob_combnations_add_status_id.sql
@@ -7,3 +7,10 @@
 ALTER TABLE public.combinations
   ADD COLUMN status_id VARCHAR(3) DEFAULT 'ACT' NOT NULL;
 
+-- set status_id not null and default to 'ACT'
+
+ALTER TABLE public.combinations
+  ALTER COLUMN status_id SET DEFAULT 'ACT'::character varying;
+
+ALTER TABLE public.combinations
+  ALTER COLUMN status_id SET NOT NULL;
