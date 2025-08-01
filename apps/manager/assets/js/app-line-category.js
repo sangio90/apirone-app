ff36f65728f38d1b8a1e9ae920bb672bc7844699
Roberto Marzialetti <roberto@marzialetti.com>
2025-08-01 20:38:42 +0200

    Eslint ❤

--- apps/manager/assets/js/app-line-category.js
+++ apps/manager/assets/js/app-line-category.js
@@ -1,86 +1,86 @@
 AP.lineCategory = AP.lineCategory || {};
 
 AP.lineCategory.fields = {
-    listRoot: $("#line-category-list-root"),
-    searchForm: $("#line-category-search-form"),
+    listRoot: $( "#line-category-list-root" ),
+    searchForm: $( "#line-category-search-form" ),
 };
 
-$(document).ready(function () {
-    if (AP.lineCategory.fields.listRoot.length) {
+$( document ).ready( function() {
+    if ( AP.lineCategory.fields.listRoot.length ) {
         AP.lineCategory.list.init();
     }
-});
+} );
 
-AP.lineCategory.list = (function () {
+AP.lineCategory.list = ( function() {
     var pub = {};
     var fields = AP.lineCategory.fields;
 
     var dataSources = {
-        items: NM.kendo.dataSource({ url: "/manager/ajax/lines/categories/" + AP.page.categoryId }),
+        items: NM.kendo.dataSource( { url: "/manager/ajax/lines/categories/" + AP.page.categoryId } ),
     };
 
     var fields = AP.lineCategory.fields;
 
-    var viewModel = kendo.observable({
+    var viewModel = kendo.observable( {
         rows: dataSources.items,
 
-        search: function (event) {
+        search: function( event ) {
             var thisForm = fields.searchForm;
 
             var params = thisForm.serializeJSON();
 
-            params.categoryId = fields.listRoot.find("[name=categoryId]").val();
+            params.categoryId = fields.listRoot.find( "[name=categoryId]" ).val();
 
-            viewModel.get("rows").read(params);
+            viewModel.get( "rows" ).read( params );
 
             return false;
         },
 
-        change: function (event) {
-            var select = $(event.currentTarget);
+        change: function( event ) {
+            var select = $( event.currentTarget );
             var categoryId = select.val();
-            var category = select.find("option:selected").data("name");
+            var category = select.find( "option:selected" ).data( "name" );
             var title = "Linee per < " + category + " >";
 
             // change url and title
-            window.history.pushState({}, "", categoryId);
+            window.history.pushState( {}, "", categoryId );
             document.title = title;
 
-            fields.listRoot.find("h2").text(title);
+            fields.listRoot.find( "h2" ).text( title );
 
-            viewModel.set("rows", NM.kendo.dataSource({ url: "/manager/ajax/lines/categories/" + categoryId }));
+            viewModel.set( "rows", NM.kendo.dataSource( { url: "/manager/ajax/lines/categories/" + categoryId } ) );
 
             return false;
         },
 
-        products: function (event) {
-            console.log("fields.listRoot", fields.listRoot);
+        products: function( event ) {
+            console.log( "fields.listRoot", fields.listRoot );
 
             var id = event.data.id;
-            var categoryId = fields.listRoot.find("[name=categoryId]").val();
+            var categoryId = fields.listRoot.find( "[name=categoryId]" ).val();
 
-            window.open("/manager/lines/" + id + "/categories/" + categoryId + "/products", "_blank").focus();
+            window.open( "/manager/lines/" + id + "/categories/" + categoryId + "/products", "_blank" ).focus();
 
             return false;
         },
 
-        attributes: function (event) {
+        attributes: function( event ) {
             /*
                 note: redirect in controller to first product
             */
 
             var id = event.data.id;
-            window.open("/manager/lines/" + id + "/attributes", "_blank").focus();
+            window.open( "/manager/lines/" + id + "/attributes", "_blank" ).focus();
 
             return false;
         },
-    });
+    } );
 
-    pub.init = function () {
-        console.log("listCategory:init");
+    pub.init = function() {
+        console.log( "listCategory:init" );
 
-        kendo.bind(AP.lineCategory.fields.listRoot, viewModel);
+        kendo.bind( AP.lineCategory.fields.listRoot, viewModel );
     };
 
     return pub;
-})();
+}() );
--- apps/manager/assets/js/app-size.js
+++ apps/manager/assets/js/app-size.js
@@ -1,22 +1,22 @@
 AP.size = AP.size || {};
 
 AP.size.fields = {
-    listRoot: $("#size-list-root"),
-    detailForm: $("#size-detail-form"),
-    searchListForm: $("#size-grid-search-form"),
+    listRoot: $( "#size-list-root" ),
+    detailForm: $( "#size-detail-form" ),
+    searchListForm: $( "#size-grid-search-form" ),
 };
 
-$(document).ready(function () {
-    if (AP.size.fields.listRoot.length) {
+$( document ).ready( function() {
+    if ( AP.size.fields.listRoot.length ) {
         AP.size.list.init();
     }
-});
+} );
 
-AP.size.list = (function () {
+AP.size.list = ( function() {
     var pub = {};
 
     var dataSources = {
-        items: NM.kendo.dataSource({ url: "/manager/ajax/sizes" }),
+        items: NM.kendo.dataSource( { url: "/manager/ajax/sizes" } ),
     };
 
     var defaultDetailForm = {
@@ -46,120 +46,120 @@ AP.size.list = (function () {
         title: "Carica dimensione",
     };
 
-    var viewModel = kendo.observable({
+    var viewModel = kendo.observable( {
         rows: dataSources.items,
         detailForm: defaultDetailForm,
 
-        resetForm: function () {
-            viewModel.set("detailForm", defaultDetailForm);
+        resetForm: function() {
+            viewModel.set( "detailForm", defaultDetailForm );
         },
 
-        search: function (event) {
+        search: function( event ) {
             var thisForm = AP.size.fields.searchListForm;
 
             var params = thisForm.serializeJSON();
 
-            viewModel.rows.read(params);
+            viewModel.rows.read( params );
 
             return false;
         },
 
-        save: function (event) {
+        save: function( event ) {
             var thisForm = AP.size.fields.detailForm;
-            var status = thisForm.find(".status");
+            var status = thisForm.find( ".status" );
 
-            status.html('<img src="/assets/main/img/ajax-loading.svg" width="20" height="20">');
+            status.html( "<img src=\"/assets/main/img/ajax-loading.svg\" width=\"20\" height=\"20\">" );
 
-            if (thisForm.valid()) {
-                NM.util.ajax({
+            if ( thisForm.valid() ) {
+                NM.util.ajax( {
                     method: "POST",
                     url: "/manager/ajax/sizes",
-                    data: JSON.stringify(viewModel.get("detailForm.data")),
+                    data: JSON.stringify( viewModel.get( "detailForm.data" ) ),
                     callback: {
-                        done: function (xhr) {
-                            if (xhr.status == "SUCCESS") {
-                                viewModel.get("rows").read();
-                                NM.util.autoHideMessage(status, "<span class='green'>Dimensione salvata</span>");
+                        done: function( xhr ) {
+                            if ( xhr.status == "SUCCESS" ) {
+                                viewModel.get( "rows" ).read();
+                                NM.util.autoHideMessage( status, "<span class='green'>Dimensione salvata</span>" );
 
-                                setTimeout(() => $("#size-detail-modal").modal("hide"), 1500);
+                                setTimeout( () => $( "#size-detail-modal" ).modal( "hide" ), 1500 );
                             }
                         },
                     },
-                });
+                } );
             }
 
             return false;
         },
 
-        new: function (event) {
+        new: function( event ) {
             this.resetForm();
 
-            NM.util.openModal($("#size-detail-modal"));
+            NM.util.openModal( $( "#size-detail-modal" ) );
         },
 
-        edit: function (event) {
-            viewModel.set("detailForm.data", event.data);
-            viewModel.set("detailForm.title", "Modifica dimensione < " + event.data.code + " >");
+        edit: function( event ) {
+            viewModel.set( "detailForm.data", event.data );
+            viewModel.set( "detailForm.title", "Modifica dimensione < " + event.data.code + " >" );
 
             var selectedCategories = [];
 
-            if (event.data.categories) {
-                for (var category of event?.data?.categories) {
-                    selectedCategories.push(category);
+            if ( event.data.categories ) {
+                for ( var category of event?.data?.categories ) {
+                    selectedCategories.push( category );
                 }
             }
 
-            viewModel.set("detailForm.data.selectedCategories", selectedCategories);
+            viewModel.set( "detailForm.data.selectedCategories", selectedCategories );
 
-            NM.util.openModal($("#size-detail-modal"));
+            NM.util.openModal( $( "#size-detail-modal" ) );
         },
 
-        delete: function (event) {
-            var checks = $("#size-grid").find("[name=selected]:checked");
+        delete: function( event ) {
+            var checks = $( "#size-grid" ).find( "[name=selected]:checked" );
 
-            if (checks.length) {
+            if ( checks.length ) {
                 var values = [];
 
-                checks.each(function () {
-                    values.push($(this).val());
-                });
+                checks.each( function() {
+                    values.push( $( this ).val() );
+                } );
 
                 var ids = values.toString();
 
-                NM.util.ajax({
+                NM.util.ajax( {
                     method: "DELETE",
                     url: "/manager/ajax/sizes",
                     data: ids,
                     callback: {
-                        done: function (xhr) {
-                            if (xhr.data.payload.hasOwnProperty("errors")) {
-                                AP.widget.notify("error", "Non riesco a cancellare tutte le dimensioni");
+                        done: function( xhr ) {
+                            if ( xhr.data.payload.hasOwnProperty( "errors" ) ) {
+                                AP.widget.notify( "error", "Non riesco a cancellare tutte le dimensioni" );
                             } else {
-                                AP.widget.notify("success", "Cancellazione avvenuta con successo");
+                                AP.widget.notify( "success", "Cancellazione avvenuta con successo" );
                             }
 
-                            var id = viewModel.get("detailForm.data.id");
+                            var id = viewModel.get( "detailForm.data.id" );
 
                             viewModel.rows.read();
                         },
                     },
-                });
+                } );
             } else {
-                AP.widget.notify("warning", "Selezionare almeno una dimensione");
+                AP.widget.notify( "warning", "Selezionare almeno una dimensione" );
             }
         },
-    });
+    } );
 
-    pub.init = function () {
-        kendo.bind(AP.size.fields.listRoot, viewModel);
+    pub.init = function() {
+        kendo.bind( AP.size.fields.listRoot, viewModel );
 
         var detailForm = AP.size.fields.detailForm;
 
-        AP.page.types.unshift({ id: "", name: "-- Seleziona il tipo" });
+        AP.page.types.unshift( { id: "", name: "-- Seleziona il tipo" } );
 
-        detailForm.validate({
-            onfocusout: function (element) {
-                $(element).valid();
+        detailForm.validate( {
+            onfocusout: function( element ) {
+                $( element ).valid();
             },
             rules: {
                 name: {
@@ -178,12 +178,12 @@ AP.size.list = (function () {
                     remote: {
                         url: "/manager/ajax/sizes/code-exists",
                         data: {
-                            id: function () {
-                                return viewModel.get("detailForm.data.id");
+                            id: function() {
+                                return viewModel.get( "detailForm.data.id" );
                             },
                         },
-                        dataFilter: function (xhr) {
-                            var json = JSON.parse(xhr);
+                        dataFilter: function( xhr ) {
+                            var json = JSON.parse( xhr );
                             return json.data == false;
                         },
                     },
@@ -206,8 +206,8 @@ AP.size.list = (function () {
                     remote: "Il codice esiste",
                 },
             },
-        });
+        } );
     };
 
     return pub;
-})();
+} () );
