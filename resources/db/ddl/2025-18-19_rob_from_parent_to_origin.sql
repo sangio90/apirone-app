83749ba74d51353366257be46b2f00fef439a311
Roberto Marzialetti <roberto@marzialetti.com>
2025-08-20 08:47:43 +0200

    DB: from parent to origin

---
+++ resources/db/ddl/2025-18-19_rob_from_parent_to_origin.sql
@@ -0,0 +1,10 @@
+﻿/*
+  2025-08-19 21:02:17
+*/
+
+ALTER TABLE public.quotation_item_zones
+  RENAME COLUMN parent_id TO origin_id;
+
+ALTER TABLE public.product_items
+  RENAME COLUMN parent_id TO origin_id;  
+  
