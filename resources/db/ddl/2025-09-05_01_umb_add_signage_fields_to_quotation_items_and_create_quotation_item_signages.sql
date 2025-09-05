c2657ebc5eba58b53ed4a31bc48f146f3a5bce23
Sangiovanni Umberto <umberto.sangiovanni@gslabs.it>
2025-09-05 17:14:41 +0200

    typo

--- resources/db/ddl/2025-09-05_01_umb_add_signage_fields_to_quotation_items_and_create_quotation_item_signages.sql
+++ resources/db/ddl/2025-09-05_01_umb_add_signage_fields_to_quotation_items_and_create_quotation_item_signages.sql
@@ -6,8 +6,8 @@ ADD COLUMN height FLOAT NULL,
 ADD COLUMN height_in_pixel FLOAT NULL,
 ADD COLUMN row_count INTEGER NULL;
 
-CREATE TABLE quotation_item_signag_rows (
-    quotation_item_signage_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+CREATE TABLE quotation_item_signage_rows (
+    quotation_item_signage_row_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     quotation_item_id UUID NOT NULL REFERENCES quotation_items(quotation_item_id) ON DELETE CASCADE,
     text_align VARCHAR(10) NOT NULL,
     content TEXT NOT NULL,
