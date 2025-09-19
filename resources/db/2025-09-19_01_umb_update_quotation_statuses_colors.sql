46d4e40ac3cebd5972e03bf9e9122734e142c525
Sangiovanni Umberto <umberto.sangiovanni@gslabs.it>
2025-09-19 11:27:43 +0200

    integrate

--- resources/db/2025-09-19_01_umb_update_quotation_statuses_colors.sql
+++ resources/db/2025-09-19_01_umb_update_quotation_statuses_colors.sql
@@ -6,5 +6,7 @@ SET color_id = CASE status_id
     WHEN 'CON' THEN 'C70'
     WHEN 'PER' THEN 'C80'
     WHEN 'EST' THEN 'C100'
+    WHEN 'NEW' THEN 'C10'
+    WHEN 'END' THEN 'C90'
 END
-WHERE status_id IN ('LAV','PRO','APR','CON','PER','EST');
+WHERE status_id IN ('LAV','PRO','APR','CON','PER','EST','NEW','END');
