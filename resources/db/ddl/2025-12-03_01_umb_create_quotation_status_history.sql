4bf4d733131c696ca3ce896d3d42298110e6dbcf
Sangiovanni Umberto <umberto.sangiovanni@gslabs.it>
2025-12-04 09:02:01 +0100

    sql creazione tabella history

---
+++ resources/db/ddl/2025-12-03_01_umb_create_quotation_status_history.sql
@@ -0,0 +1,21 @@
+CREATE TABLE quotation_status_history (
+    quotation_status_history_id SERIAL,
+        quotation_id UUID STORAGE PLAIN NOT NULL,
+        account_id UUID NOT NULL,
+        status_id VARCHAR(3) NOT NULL,
+        created_at timestamp without time zone DEFAULT now(),
+        PRIMARY KEY (quotation_status_history_id),
+        FOREIGN KEY (quotation_id) REFERENCES quotations(quotation_id),
+        FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+        FOREIGN KEY (status_id) REFERENCES statuses(status_id)
+);
+
+ALTER TABLE files
+  ADD COLUMN quotation_status_history_id INTEGER;
+
+ALTER TABLE files
+  ADD CONSTRAINT files_quotation_status_history_id_fk FOREIGN KEY (quotation_status_history_id)
+    REFERENCES quotation_status_history(quotation_status_history_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    NOT DEFERRABLE; 
