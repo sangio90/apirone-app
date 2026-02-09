76279a7f25b7e195b2b9ceb21965d19af16657cf
Roberto Marzialetti <roberto@marzialetti.com>
2026-02-09 14:16:04 +0100

    Sql

---
+++ resources/db/ddl/2026-02-05_01_rob_role_limits_for_quotations.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE membership.roles
+ADD COLUMN quotation_max_amount NUMERIC(10, 2);
+
+ALTER TABLE membership.roles
+ADD COLUMN quotation_max_discount NUMERIC(10, 2);
---
+++ resources/db/ddl/2026-02-05_02_rob_position_id_in_quotation_items.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE public.quotation_items
+ADD COLUMN quotation_zone_position_id INTEGER;
+
+ALTER TABLE public.quotation_items
+ADD CONSTRAINT quotation_items_zone_position_id_fk FOREIGN KEY (quotation_zone_position_id) REFERENCES public.quotation_zone_positions (quotation_zone_position_id) ON DELETE NO ACTION ON UPDATE CASCADE NOT DEFERRABLE;
+
+ALTER TABLE public.quotation_zone_positions
+ADD COLUMN code VARCHAR(15);
