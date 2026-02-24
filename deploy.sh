58be581180e1b11351fa990338fc69101765a321
sangio90 <guido.sangiovanni@gslabs.it>
2026-02-24 18:15:04 +0100

    typo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+git config --global --add safe.directory /var/www/vhosts/stage_apirone_cc_git/html
 git pull origin master
 touch /var/log/rilascio_stage.txt
 echo "Rilascio stage $(date)" >> /var/log/rilascio_stage.txt
