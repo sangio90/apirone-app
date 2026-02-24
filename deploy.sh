973ab7761d46535138198cc53e3c8030efe02e29
sangio90 <guido.sangiovanni@gslabs.it>
2026-02-24 18:19:59 +0100

    fix path log rilascio

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -e
 git config --global --add safe.directory /var/www/vhosts/stage_apirone_cc_git/html
 git pull origin master
-touch /var/log/rilascio_stage.txt
-echo "Rilascio stage $(date)" >> /var/log/rilascio_stage.txt
+touch ~/rilascio_stage.txt
+echo "Rilascio stage $(date)" >> ~/rilascio_stage.txt
