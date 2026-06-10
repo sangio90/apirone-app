7faf8eeab1c3843cf3a0bc8dac9e1bfbb21fc056
e-magon <emagon@emagon.xyz>
2026-06-10 16:06:20 +0200

    fix: spinner caricamento in dialog nuova placca

--- apps/manager/assets/js/app-quotation-plate-vue.js
+++ apps/manager/assets/js/app-quotation-plate-vue.js
@@ -2206,6 +2206,7 @@ AP.plate.modal = ( function() {
      * @param {Function} [onSave] - Callback opzionale da eseguire dopo il salvataggio.
      */
     pub.new = async function( onSave ) {
+        AP.loading.show();
         NM.util.openModal( AP.plate.fields.modalRoot );
 
         await mountVue();
@@ -2280,6 +2281,7 @@ AP.plate.modal = ( function() {
         window.vm.fruitSearchTerm = "";
 
         plateResponse = null;
+        AP.loading.hide();
     };
 
     /**
